e2e666ef65761d0b6e281687d8a67b1de47c1366
Cyprian Kings <kiamacyprian6@gmail.com>
2025-10-19 15:19:32 +0300

    fix: turning off debug and adding ssl redirecct and SESSION_COOKIE_SECURE settings for security in deployment.

--- borrowedwords/settings.py
+++ borrowedwords/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e6u(1s+6=v2mx^=sfa2x7!s5dxhlj$7a%4rh4y5#(i-n2+*=-1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Security settings
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
