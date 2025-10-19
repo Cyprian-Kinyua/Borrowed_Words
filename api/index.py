86b218b40c046e56470486908df30eda232cbd46
Cyprian Kings <kiamacyprian6@gmail.com>
2025-10-19 21:49:06 +0300

    fix: define app in api/index.py for vercel

--- api/index.py
+++ api/index.py
@@ -6,4 +6,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "borrowedwords.settings")
 
 # Get the WSGI application
-application = get_wsgi_application()
+app = get_wsgi_application()
